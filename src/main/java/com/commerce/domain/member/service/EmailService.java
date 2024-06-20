4bf4bba8a95a3953a399be4a042763276d07fd4c
DZOOOOO <dzpro0327@naver.com>
2024-06-21 07:06:36 +0900

    member-mail add

---
+++ src/main/java/com/commerce/domain/member/service/EmailService.java
@@ -0,0 +1,36 @@
+package com.commerce.domain.member.service;
+
+import com.commerce.domain.web.member.dto.EmailSendDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Transactional
+    public void sendMail(EmailSendDto dto) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        StringBuilder sb = new StringBuilder();
+        String email = "email=" + dto.getTo();
+        String token = "&token=" + UUID.randomUUID().toString().substring(0, 10);
+        String valid = "&" + dto.getText();
+        String url = "http://localhost:8080/api/v1/member/confirm-email?";
+        String result = sb.append(url).append(email).append(token).append(valid).toString();
+
+        message.setTo(dto.getTo());
+        message.setSubject(dto.getSubject());
+        message.setText(result);
+        message.setFrom("devpet0327@gmail.com"); // 보내는 사람 이메일 주소
+
+        mailSender.send(message);
+    }
+
+}
---
+++ src/main/java/com/commerce/domain/web/member/dto/EmailSendDto.java
@@ -0,0 +1,20 @@
+package com.commerce.domain.web.member.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EmailSendDto {
+
+    // 받는 사람.
+    private String to;
+
+    // 메일 제목
+    private String subject;
+
+    // 메일 내용.
+    private String text;
+}
